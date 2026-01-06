37927fe6606b256ac74984f069e05aebd240f0ca
Saurav Kumar <zoroduke007@gmail.com>
2026-01-07 03:35:22 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,39 +2,37 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
 import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env.js";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middleware/errorHandler.js";
 import catchError from "./utils/catchError.js";
-
+import { OK } from "./constants/http.js";
+import authRoutes from "./routes/Auth.route.js"
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
+app.use(
+  cors({
     origin: APP_ORIGIN,
     credentials: true,
-}));
+  })
+);
 
 app.use(cookieParser());
 
-
-
-app.get("/", catchError(  async(req, res,next) => {
-
-    throw new Error("we testing the errror dynamic");
-    res.status(200).json({
-      status:"healthy"
+app.get(
+  "/",
+  catchError(async ( req,res,next) => {
+    return res.status(OK).json({
+      status: "healthy",
     });
-
-    
   })
- 
 );
+app.use("/auth",authRoutes)
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
-    connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
+  connectDB();
 });
-
